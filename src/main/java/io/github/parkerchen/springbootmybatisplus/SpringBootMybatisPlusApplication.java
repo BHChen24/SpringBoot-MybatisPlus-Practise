047604fa170abfdc4d7f5f437dc7afd0eaaa8b62
Baihua Chen <bchen102@myseneca.ca>
2024-11-01 00:15:27 -0400

    Push to remote

--- src/main/java/io/github/parkerchen/springbootmybatisplus/SpringBootMybatisPlusApplication.java
+++ src/main/java/io/github/parkerchen/springbootmybatisplus/SpringBootMybatisPlusApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootMybatisPlusApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootMybatisPlusApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMybatisPlusApplication.class, args);
+    }
 
 }
--- src/main/java/io/github/parkerchen/springbootmybatisplus/entity/user.java
+++ src/main/java/io/github/parkerchen/springbootmybatisplus/entity/user.java
@@ -1,2 +1,27 @@
-package io.github.parkerchen.springbootmybatisplus.entity;public class user {
+package io.github.parkerchen.springbootmybatisplus.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@TableName("user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class user {
+    @TableId(type = IdType.AUTO)
+    private Long id;
+    @TableField("name")
+    private String name;
+    @TableField("age")
+    private Integer age;
+    @TableField("email")
+    private String email;
+
 }
